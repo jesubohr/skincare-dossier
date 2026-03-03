49cd92d4395b5acb2b8faced9f291644aa2ad97a
Jesus Borrero <jesufer2018@hotmail.com>
2026-03-03 00:31:53 -0500

    feat: add theme toggler

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useTheme } from "next-themes"
+import { Sun, Moon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+
+const iconStyles = "group-hover:text-primary transition-colors"
+
+export function ThemeToggle() {
+  const { resolvedTheme, setTheme } = useTheme()
+
+  function toggleTheme() {
+    const isAppearanceTransition =
+      // @ts-expect-error - document.startViewTransition is not yet in the official TS types
+      document.startViewTransition !== undefined && !window.matchMedia("(prefers-reduced-motion: reduce)").matches
+
+    if (!isAppearanceTransition) {
+      setTheme(resolvedTheme === "dark" ? "light" : "dark")
+      return
+    }
+
+    // @ts-expect-error - document.startViewTransition is not yet in the official TS types
+    document.startViewTransition(async () => {
+      setTheme(resolvedTheme === "dark" ? "light" : "dark")
+    })
+  }
+
+  return (
+    <Button size="icon" variant="ghost" onClick={toggleTheme} className="group h-9 w-9 hover:bg-primary/10 dark:hover:bg-primary/30">
+      {resolvedTheme === "dark" ? <Moon className={iconStyles} suppressHydrationWarning /> : <Sun className={iconStyles} suppressHydrationWarning />}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
