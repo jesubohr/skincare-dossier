4d56db37080df17d77dddc7308c65e1d444bcd10
Jesus Borrero <jesufer2018@hotmail.com>
2026-03-05 02:20:55 -0500

    feat: add clerk for auth

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,19 +2,23 @@
 
 import { useEffect } from "react"
 import { ThemeProvider } from "next-themes"
+import { ConvexReactClient } from "convex/react"
+import { ClerkProvider, useAuth } from "@clerk/nextjs"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
 import { useUserStore } from "@/lib/store/user-store"
-import { ConvexProvider, ConvexReactClient } from "convex/react"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <ConvexProvider client={convex}>
-        <UserProvider>
-          <div className="w-full min-h-screen">{children}</div>
-        </UserProvider>
-      </ConvexProvider>
+      <ClerkProvider>
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <UserProvider>
+            <div className="w-full min-h-screen">{children}</div>
+          </UserProvider>
+        </ConvexProviderWithClerk>
+      </ClerkProvider>
     </ThemeProvider>
   )
 }
